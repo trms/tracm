b5188ae72d74a260f5e7259b848d6f57813c6509
John Reilly <jr@trms.com>
2010-02-06 02:31:02 +0800

    updating license, adding copy of GPLv3

--- tracm/Settings.cs
+++ tracm/Settings.cs
@@ -1,6 +1,9 @@
 ﻿/*
 //-------------------------------------------------------------------------------//
-This file is part of tracm.
+This file is part of tracm, a client for the ACM Shared Content Server.
+Copyright (C) 2010 Tightrope Media Systems
+http://www.trms.com
+http://labs.trms.com
 
 tracm is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
